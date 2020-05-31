32cc7bacf95a82336194ec8abd56bab6e7003476
radhi-ops <harrarradhi@gmail.com>
2020-06-01 01:38:15 +0200

    new commit

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,12 +2,25 @@ import React from "react";
 import { Card, Col } from "react-bootstrap";
 
 const Card1 = props => {
+   const converttostar = () => {
+    let list = ["☆", "☆", "☆", "☆", "☆"];
+
+    for (let i = 0; i < props.data.rating; i++) {
+      list[i] = <span style={{ fontSize: "22px" ,color:"yellow"}}>★</span>;
+    }
+    for (let i =  props.data.rating; i < 5; i++) {
+      list[i] = <span style={{ fontSize: "22px" ,color:"black"}}>☆</span>;
+    }
+    return list;
+    //  var str ="★" ;
+     //  return str.toString().repeat(props.data.rating).concat('☆'.repeat(5-props.data.rating))
+   }
   return (
     <Col xl={3} style={{ margin: "2%" }}>
       <Card>
         <Card.Body>
           <Card.Title>Title : {props.data.title}</Card.Title>
-          <Card.Text>Rating : {props.data.rating}</Card.Text>
+          <Card.Text>Rating : {converttostar()} </Card.Text>
         </Card.Body>
       </Card>
     </Col>
@@ -15,3 +28,5 @@ const Card1 = props => {
 };
 
 export default Card1;
+
+// ★☆
--- src/Components/Movie-container.js
+++ src/Components/Movie-container.js
@@ -4,50 +4,86 @@ import { Modal, Button, Form, Row, Col } from "react-bootstrap";
 const List = [
   {
     title: "Western",
-    rating: 6
+    rating: 4
   },
   {
     title: "Titanic",
-    rating: 8
+    rating: 5
   },
   {
     title: "Avengers",
-    rating: 7.5
+    rating: 2
   },
   {
     title: "Thor",
-    rating: 7
+    rating: 5
   },
   {
     title: "Gladiator",
-    rating: 8.5
+    rating: 4
   }
 ];
+const starsList = ["☆", "☆", "☆", "☆", "☆"];
 const MovieContainer = () => {
   const [searchedValue, setSearch] = useState("");
+  const [searchedRating, setSearchRating] = useState(0);
   const [listData, setData] = useState(List);
+  const [show, setShow] = useState(false);
+  const [title, setTitle] = useState("");
+  const [rating, setRating] = useState("");
+  const [stars, setStars] = useState(starsList);
   const filterdata = value => {
     let filtred = List.filter(elem => {
-      return (
-        (elem.title.toLowerCase().indexOf(value.toLowerCase()) > -1) |
-        (elem.rating
-          .toString()
-          .toLowerCase()
-          .indexOf(value.toLowerCase()) >
-          -1)
-      );
+      if (searchedRating === 0) {
+        return elem.title.toLowerCase().indexOf(value.toLowerCase()) > -1;
+      }
+      else
+      {
+        return (
+          elem.title.toLowerCase().indexOf(value.toLowerCase()) > -1 &&
+          elem.rating.toString().indexOf(searchedRating) > -1
+        );
+      }
     });
     setData(filtred);
     setSearch(value);
   };
-  const [show, setShow] = useState(false);
-  const [title, setTitle] = useState("");
-  const [rating, setRating] = useState("");
+  const filterbyrating = position => {
+    let list = ["☆", "☆", "☆", "☆", "☆"];
+
+    for (let i = 0; i <= position; i++) {
+      list[i] = <span style={{ fontSize: "22px" ,color:"red"}}>★</span>;
+    }
+    setStars(list);
+    setSearchRating(position + 1);
+
+    let filtred = List.filter(elem => {
+      if (searchedValue === "") {
+        if (position === -1) {
+          return elem;
+        } else {
+          return elem.rating.toString().indexOf(position + 1) > -1;
+        }
+      } else {
+        if (position === -1) {
+          return (
+            elem.title.toLowerCase().indexOf(searchedValue.toLowerCase()) > -1
+          );
+        } else {
+          return (
+            elem.rating.toString().indexOf(position + 1) > -1 &&
+            elem.title.toLowerCase().indexOf(searchedValue.toLowerCase()) > -1
+          );
+        }
+      }
+    });
+    setData(filtred);
+  };
   const handleClose = () => setShow(false);
   const handleSave = () => {
     List.push({ title: title, rating: rating });
-    console.log(List)
-    setData(List)
+    console.log(List);
+    setData(List);
     handleClose();
   };
   const addMovie = () => {
@@ -72,11 +108,17 @@ const MovieContainer = () => {
             />
             <Form.Label>Movie Rating</Form.Label>
             <Form.Control
-              type="text"
+              type="number"
+              max="5"
+              min="0"
               value={rating}
               placeholder="Enter Movie rating"
               onChange={e => {
-                setRating(e.target.value);
+                if (e.target.value < 0 || e.target.value > 5) {
+                  setRating(0);
+                } else {
+                  setRating(e.target.value);
+                }
               }}
             />
           </Form.Group>
@@ -91,23 +133,41 @@ const MovieContainer = () => {
         </Modal.Footer>
       </Modal>
       <div style={{ margin: "2%" }}>
-      <Row>
-        <Col xl={6}>
-        <Form.Control
-          type="text"
-          value={searchedValue}
-          placeholder="filter by movie name or rating "
-          onChange={e => {
-            filterdata(e.target.value);
-          }}
-          style={{ marginRight: "2%" }}
-        ></Form.Control>
-        </Col>
-        <Col>
-        <Button type="button" onClick={() => addMovie()}>
-          Add movie
-        </Button>
-        </Col>
+        <Row>
+          <Col xl={4}>
+            <Form.Control
+              type="text"
+              value={searchedValue}
+              placeholder="filter by movie name"
+              onChange={e => {
+                filterdata(e.target.value);
+              }}
+              style={{ marginRight: "2%" }}
+            ></Form.Control>
+          </Col>
+          <Col>
+            {stars.map((star, i) => {
+              return (
+                <span
+                  onMouseOver={() => {
+                    filterbyrating(i);
+                  }}
+                  
+                >
+                  {" "}
+                  {star}
+                </span>
+              );
+            })}
+            <span style={{ marginLeft: "2%" }}>
+              <i class="fa fa-undo" onClick={() => filterbyrating(-1)}></i>
+            </span>
+          </Col>
+          <Col>
+            <Button type="button" onClick={() => addMovie()}>
+              Add movie
+            </Button>
+          </Col>
         </Row>
       </div>
       <MovieList data={listData} />
